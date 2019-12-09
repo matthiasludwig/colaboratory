bde2c1ea9232247875bea86eb30a43495a10c1ab
Matthias <matthiasludwig@me.com>
2019-12-09 18:18:44 +0100

    csbase: Added scripts from Parth IV (radamsa)

---
+++ csbase/scripts/radamsa-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Need to install radamsa in ~/Downloads/radamsa
+
+# For Setup only
+#mkdir output-samples
+#mkdir imgmgck-output
+
+~/Downloads/radamsa/bin/radamsa -n 100 -o output-samples/img-%n.jpg ~/Pictures/matthias.jpg
+
+for f in output-samples/*
+do
+    convert "$f" -resize 99% imgmgck-output/$(basename "$f" | cut -d . -f1).png
+    test $? -gt 127 && break
+done
