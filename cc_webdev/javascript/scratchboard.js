773c56d56c0d74d6374a5740dc5c0fdab246bd5b
Matthias <matthiasludwig@me.com>
2020-01-03 23:46:48 +0100

    Added code from excercises JS & HTML

--- cc_webdev/javascript/scratchboard.js
+++ cc_webdev/javascript/scratchboard.js
@@ -2,3 +2,11 @@
 const helloWorld = () => console.log("Hello World!");
 
 helloWorld();
+
+// Example of function
+
+const testFunc = function() {
+    console.log("Hello World, from testFunc");
+}
+
+testFunc();
