eb2d5a7ad9678e636baf595a4db7caf3920e5284
Matthias <matthiasludwig@me.com>
2020-01-05 12:44:52 +0100

    Reviewed JS lessons

--- cc_webdev/javascript/scratchboard.js
+++ cc_webdev/javascript/scratchboard.js
@@ -1,12 +1,14 @@
-// Example of anymous function
+// Example of a arrow function
 const helloWorld = () => console.log("Hello World!");
 
 helloWorld();
 
-// Example of function
+// Example of an anonymous function
 
 const testFunc = function() {
     console.log("Hello World, from testFunc");
 }
 
 testFunc();
+
+console.log("Teststring is " + "TestString".length + " chars long")
