3ab6270ad93b58a7018bcf93d4a2e23323cf5dd9
Matthias <matthiasludwig@me.com>
2020-01-23 22:14:42 +0100

    cc_webdev: whaletalk added solution from lesson

--- cc_webdev/projects/whaletalk/app.js
+++ cc_webdev/projects/whaletalk/app.js
@@ -1,16 +1,21 @@
-let input = 'Hello Whale World!';
+let input = 'a whale of a deal';
 
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 
 let resultArray = []
 
 for (let i = 0; i < input.length; i++) {
-  for (let j = 0; j < vowels.length; i++) {
-    if (input[i] === vowels[j]) {
-      resultArray.push(input[i]);
+  for (let j = 0; j < vowels.length; j++) {
+    if(input[i] === vowels[j]) {
+      resultArray.push(input[i])
     }
   }
+  if (input[i] === 'e') {
+      resultArray.push(input[i])
+    }
+  else if (input[i] === 'u') {
+      resultArray.push(input[i])
+    }
 }
 
-console.log(resultArray);
-console.log("Hello World!");
+console.log(resultArray.join('').toUpperCase());
