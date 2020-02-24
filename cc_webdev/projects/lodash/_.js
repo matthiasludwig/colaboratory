f571cb7fa907741ca8f7da069da12462d956aeca
Matthias <matthiasludwig@me.com>
2020-02-24 22:06:32 +0100

    cc_webdev: finished clamp.js in lodash.js

--- cc_webdev/projects/lodash/_.js
+++ cc_webdev/projects/lodash/_.js
@@ -1,7 +1,14 @@
-let _ = {};
-
-
-
-
-// Do not write or modify code below this line.
-module.exports = _;
+let _ = {
+    clamp(number, lower, upper) {
+      let lowerClampedValue = Math.max(number, lower);
+      let clampedValue = Math.min(lowerClampedValue, upper);
+      
+      return clampedValue;
+    }
+  };
+  
+  
+  
+  
+  // Do not write or modify code below this line.
+  module.exports = _;
