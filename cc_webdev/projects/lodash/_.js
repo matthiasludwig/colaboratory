7ec756d881f86def25ec3a0452a099e38cd0b4ab
Matthias <matthiasludwig@me.com>
2020-02-26 22:42:08 +0100

    cc_webdev: Finished inRange function

--- cc_webdev/projects/lodash/_.js
+++ cc_webdev/projects/lodash/_.js
@@ -1,14 +1,37 @@
 let _ = {
-    clamp(number, lower, upper) {
-      let lowerClampedValue = Math.max(number, lower);
-      let clampedValue = Math.min(lowerClampedValue, upper);
-      
-      return clampedValue;
+  clamp(number, lower, upper) {
+    let lowerClampedValue = Math.max(number, lower);
+    let clampedValue = Math.min(lowerClampedValue, upper);
+    
+    return clampedValue;
+  },
+  inRange(number, start, end) {
+    // Check if end is undefined
+    if (end === undefined) {
+      end = start;
+      start = 0;
     }
-  };
-  
-  
-  
-  
-  // Do not write or modify code below this line.
-  module.exports = _;
+    // Swap if start is greater than end
+    if (start > end) {
+      let temp = start;
+      start = end;
+      end = temp;
+    }
+    // Check if number is in Range of start and end
+    let inRange;
+    if (number >= start && number < end) {
+      inRange = true;
+    }
+    else {
+      inRange = false;
+    }
+    
+    return inRange;
+  }
+};
+
+
+
+
+// Do not write or modify code below this line.
+module.exports = _;
