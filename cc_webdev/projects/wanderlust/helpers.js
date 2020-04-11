c7c0108248a9c339288384413998b4ecc6ced201
Matthias <matthiasludwig@me.com>
2020-04-11 17:56:10 +0200

    cc_webdev: Cleanup & new functions (WIP)

--- cc_webdev/projects/wanderlust/helpers.js
+++ cc_webdev/projects/wanderlust/helpers.js
@@ -1,18 +1,18 @@
-const createVenueHTML = (name, location, iconSource) => {
-    console.log(name, location, iconSource);
+const createVenueHTML = (name, location, url, iconSource) => {
     return `<h2>${name}</h2>
     <img class="venueimage" src="${iconSource}"/>
     <h3>Address:</h3>
     <p>${location.address}</p>
     <p>${location.city}</p>
-    <p>${location.country}</p>`;
+    <p>${location.country}</p>
+    <p>${url}</p>`;
   }
   
   const createWeatherHTML = (currentDay) => {
-    console.log(currentDay)
     return `<h2>${weekDays[(new Date()).getDay()]}</h2>
           <h2>Temperature: ${kelvinToFahrenheit(currentDay.main.temp)}&deg;F</h2>
           <h2>Condition: ${currentDay.weather[0].description}</h2>
+          <h2>Wind: ${currentDay.wind.speed}</h2>
         <img src="https://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png">`;
   }
   
