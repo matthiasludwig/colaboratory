c9215bc81639ee6961abaca4c15a02767d7c6949
Matthias <matthiasludwig@me.com>
2020-11-17 16:22:36 +0100

    cc_fullstack: Finished mystery-organism project

--- cc_fullstack/projects/mystery-organism-starter/main.js
+++ cc_fullstack/projects/mystery-organism-starter/main.js
@@ -13,10 +13,51 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+function pAequorFactory(num, bases) {
+  return {
+    specimenNum: num,
+    dna: bases,
+    mutate: function() {
+      this.dna = mockUpStrand();
+    },
+    compareDNA: function(pAequor) {
+      let identical = 0;
+      const baseLen = this.dna.length;
+
+      for (let i = 0; i < baseLen; i++) {
+        (this.dna[i] === pAequor.dna[i]) ? identical += 1 : null ;
+      }
+      const ratio = Math.floor(100 * (identical / baseLen));
+
+      return `specimen #${this.specimenNum} and specimen #${pAequor.specimenNum} have ${ratio}% DNA in common.`;
+    },
+    willLikelySurvive: function() {
+      const surviveArr = this.dna.filter(item => (item === 'C' || item === 'G'));
+      const ratio = Math.floor(100 * (surviveArr.length / this.dna.length));
+
+      return (ratio >= 60) ? true : false;
+    }
+  }
+}
+
+const test1 = pAequorFactory(1, mockUpStrand());
+const test2 = pAequorFactory(2, mockUpStrand());
+console.log(test1);
+test1.mutate();
+console.log(test1);
+
+console.log(test1.compareDNA(test2));
+console.log(test1.willLikelySurvive());
 
 
+function createThirty() {
+  let thirtyArr = [];
+  while (thirtyArr.length < 30) {
+    let candidate = pAequorFactory(thirtyArr.length, mockUpStrand());
+    (candidate.willLikelySurvive()) ? thirtyArr.push(candidate) : null ;
+  }
+  return thirtyArr;
+}
 
-
-
-
-
+const testArr = createThirty();
+console.log(testArr.length);
