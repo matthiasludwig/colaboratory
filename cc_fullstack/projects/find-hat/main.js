6968af7655127d0d1675fa36d491ce0b00ef54b5
Matthias <matthiasludwig@me.com>
2021-01-07 23:24:01 +0100

    cc_fullstack: Added eslint with airbnb guidelines

--- cc_fullstack/projects/find-hat/main.js
+++ cc_fullstack/projects/find-hat/main.js
@@ -1,23 +1,17 @@
-const prompt = require('prompt-sync')({sigint: true});
-
-const hat = '^';
-const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
+const prompt = require('prompt-sync')({ sigint: true });
 
 class Field {
-  constructor(field) {
-    this.field = field;
-    this.rowLength = field[0].length;
-    this.colLength = field.length;
-    this._done = false;
+  constructor(map) {
+    this.field = map;
+    this.rowLength = map[0].length;
+    this.colLength = map.length;
+    this.done = false;
   }
 
-  //Getter & Setter
+  // Getter & Setter
   get status() {
-      return this._done;
+    return this.done;
   }
-
 
   // Helper function
   print() {
@@ -26,13 +20,14 @@ class Field {
       console.log(i.join(''));
     }
   }
+
   findLocation() {
-    let result = undefined;
+    let result;
 
     for (const i in this.field) {
-      let index = this.field[i].findIndex(char => char === '*')
+      const index = this.field[i].findIndex((char) => char === '*');
       if (index != -1) {
-        result = [Number(i), index]
+        result = [Number(i), index];
         break;
       }
     }
@@ -41,82 +36,82 @@ class Field {
 
   // Movement
   checkBounds(newPosition) {
-    let lowerRightBound = (newPosition[0] < this.rowLength && newPosition[1] < this.colLength) ? true : false;
-    let upperLeftBound = (newPosition[0] >= 0 && newPosition[1] >= 0) ? true : false;
+    const lowerRightBound = !!(newPosition[0] < this.rowLength && newPosition[1] < this.colLength);
+    const upperLeftBound = !!(newPosition[0] >= 0 && newPosition[1] >= 0);
     return (lowerRightBound && upperLeftBound);
   }
+
   checkEnd(newPosition) {
-      if (this.field[newPosition[0]][newPosition[1]] === '0') {
-          this._done = true;
-          console.log("Game Over: You fell in the hole!")
-          return false;
-      }
-      else if (this.field[newPosition[0]][newPosition[1]] === '^') {
-          this._done = true;
-          console.log("You won! Found the hat!");
-          return false;
-      }
-  }
-  changePosition(change) {
-    let currentPosition = this.findLocation();
-    let newPosition = [(currentPosition[0] + change[0]), (currentPosition[1] + change[1])];
-    if (this.checkBounds(newPosition)) {
-        if (!(this.checkEnd(newPosition))) {
-            this.field[currentPosition[0]][currentPosition[1]] = '░';
-            this.field[newPosition[0]][newPosition[1]] = '*';
-        }
+    if (this.field[newPosition[0]][newPosition[1]] === '0') {
+      this.done = true;
+      console.log('Game Over: You fell in the hole!');
+      return false;
     }
-    else {
-        console.log("Out of bounds!");
+    if (this.field[newPosition[0]][newPosition[1]] === '^') {
+      this.done = true;
+      console.log('You won! Found the hat!');
+      return false;
     }
   }
 
-    //Generate Field
-    static generateField(row, col, prob) {
-        let map = []
-        for (let r = row; r > 0; r--) {
-            let rowArr = []
-            for (let c = col; c > 0; c--) {
-                rowArr.push(((Math.random() > prob) ? '░' : '0'));
-            }
-            map.push(rowArr);
-        }
-        map[0][0] = '*';
-        map[row - 1][col - 1] = "^";
-        return map;
+  changePosition(change) {
+    const currentPosition = this.findLocation();
+    const newPosition = [(currentPosition[0] + change[0]), (currentPosition[1] + change[1])];
+    if (this.checkBounds(newPosition)) {
+      if (!(this.checkEnd(newPosition))) {
+        this.field[currentPosition[0]][currentPosition[1]] = '░';
+        this.field[newPosition[0]][newPosition[1]] = '*';
       }
+    } else {
+      console.log('Out of bounds!');
+    }
+  }
+
+  // Generate Field
+  static generateField(row, col, prob) {
+    const map = [];
+    for (let r = row; r > 0; r--) {
+      const rowArr = [];
+      for (let c = col; c > 0; c--) {
+        rowArr.push(((Math.random() > prob) ? '░' : '0'));
+      }
+      map.push(rowArr);
+    }
+    map[0][0] = '*';
+    map[row - 1][col - 1] = '^';
+    return map;
+  }
 }
 
 // Start the Program
-const checkInput = (char, field) => {
-    switch(char) {
-        case 'd':
-            field.changePosition([1, 0])
-            break;
-        case 'u':
-            field.changePosition([-1, 0])
-            break;
-        case 'l':
-            field.changePosition([0, -1])
-            break;
-        case 'r':
-            field.changePosition([0, 1])
-            break;
-        default:
-            console.log("Please make a valid choice (q, d, u, l, r)");
-    }  
-}
+const checkInput = (char, gameMap) => {
+  switch (char) {
+    case 'd':
+      gameMap.changePosition([1, 0]);
+      break;
+    case 'u':
+      gameMap.changePosition([-1, 0]);
+      break;
+    case 'l':
+      gameMap.changePosition([0, -1]);
+      break;
+    case 'r':
+      gameMap.changePosition([0, 1]);
+      break;
+    default:
+      console.log('Please make a valid choice (q, d, u, l, r)');
+  }
+};
 
-const field = new Field(Field.generateField(5, 5, .2))
+const field = new Field(Field.generateField(5, 5, 0.2));
 
-while(!field.status) {
+while (!field.status) {
   field.print();
-  let movementInput = prompt("Which way?");
+  const movementInput = prompt('Which way?');
 
   if (movementInput === 'q') {
-      field._done = true;
-  }
-  else {
+    field.done = true;
+  } else {
     checkInput(movementInput, field);
   }
 }
